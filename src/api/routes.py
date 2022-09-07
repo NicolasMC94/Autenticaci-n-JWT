ba51f79b2b72d36492f1c1882f2a7ebf0ccc4fbd
NicolasMC94 <mcnicolas.94@gmail.com>
2022-09-07 19:24:34 +0000

    endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,35 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    user = User.query.filter_by(email = data.get("email")).first()
+    if user is not None:
+        return "Este usuario ya existe", 404
+    new_user = User(
+        email = data.get("email"),
+        password = data.get("password"),
+        is_active = True
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email = data.get("email"), password = data.get("password")).first()
+    if user is None:
+        return "Usuario incorrecto", 401
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    
+    return jsonify({"id": user.id, "username": user.username }), 200
